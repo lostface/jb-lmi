83f0877894c8066c20534fba436de69e254eef4d
Zoltan Feher <lostface@fastmail.fm>
2016-10-15 17:54:27 +0200

    Add initial board state

---
+++ app/config.js
@@ -0,0 +1,4 @@
+export const BOARD_SIZE = 3;
+export const CELL_BG_COLOR_DEFAULT = '#fff';
+export const CELL_BG_COLOR_WON = '#0f0';
+export const CELL_MARK_DEFAULT = '';
--- app/containers/app.js
+++ app/containers/app.js
@@ -1,11 +1,39 @@
 import * as React from 'react';
+import * as R from 'ramda';
+import { BOARD_SIZE, CELL_BG_COLOR_DEFAULT, CELL_MARK_DEFAULT } from '../config';
+import { Board } from '../components';
+
+const getDefaultBoardData = boardSize =>
+  R.map(
+    index => ({
+      id: index,
+      mark: CELL_MARK_DEFAULT,
+      bgColor: CELL_BG_COLOR_DEFAULT,
+    }),
+    R.range(0, boardSize * boardSize)
+  );
 
 export default React.createClass({
   render() {
+    const { boardData } = this.state;
+
     return (
-      <div>
-        <p>App</p>
+      <div
+          className="app-container"
+          style={{
+            boxSizing: 'border-box',
+            margin: 0,
+            padding: 0,
+            border: 'none',
+          }}>
+        <Board size={BOARD_SIZE} data={boardData} />
       </div>
     );
   },
+
+  getInitialState() {
+    return {
+      boardData: getDefaultBoardData(BOARD_SIZE),
+    };
+  }
 });
